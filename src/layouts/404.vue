5883ce4eab4283986e03eff1bb764808b2ae154e
sunshj <sunshj317@qq.com>
2023-09-01 17:24:10 +0800

    feat(404): 增加404页面

---
+++ src/layouts/404.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <el-result
+      :subTitle="`抱歉，访问地址 ${$route.path} 不存在`"
+      title="404 Not Found"
+      icon="error"
+    >
+      <template #extra>
+        <el-button type="primary" @click="$router.back()">返回</el-button>
+      </template>
+    </el-result>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  padding-bottom: 10vh;
+}
+</style>
