f62d3bf831db044ddc68b95216f023a88944f109
sunshj <sunshj317@qq.com>
2024-09-23 17:36:04 +0800

    feat(utils): add `createAsyncValidator` helper function

--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -1,26 +1,32 @@
-export function isUserName(_rule: any, value: string, callback: Function) {
-  if (value.trim() === '') return callback(new Error('请输入用户名'))
+import type { FormItemRule } from 'element-plus'
+
+export function createAsyncValidator(validator: FormItemRule['validator']) {
+  return validator
+}
+
+export const isUserName = createAsyncValidator((_rule, value, callback) => {
+  if (!value.trim()) return callback(new Error('请输入用户名'))
   const regName = /^[\w\u4E00-\u9FA5-]{2,10}$/
   if (regName.test(value)) {
     return callback()
   }
   callback(new Error('长度为2到10字，且不得包含特殊字符'))
-}
+})
 
-export function isPassword(_rule: any, value: string, callback: Function) {
+export const isPassword = createAsyncValidator((_rule, value, callback) => {
   if (value.trim() === '') return callback(new Error('请输入密码'))
   const regPass = /^[\dA-Z]{6,15}$/i
   if (regPass.test(value)) {
     return callback()
   }
   callback(new Error('长度为6到15字，且不得包含特殊字符'))
-}
+})
 
-export function isEmail(_rule: any, value: string, callback: Function) {
+export const isEmail = createAsyncValidator((_rule, value, callback) => {
   if (value.trim() === '') return callback(new Error('请输入邮箱'))
   const regEmail = /^[\dA-Z\u4E00-\u9FA5]+@[\w-]+(?:\.[\w-]+)+$/i
   if (regEmail.test(value)) {
     return callback()
   }
   callback(new Error('请输入合法的邮箱地址'))
-}
+})
