ee13a1282e42b756f429142f09c94b420b5039cc
sunshj <47186980+sunshj@users.noreply.github.com>
2025-02-16 21:15:29 +0800

    chore: update deps

--- eslint.config.js
+++ eslint.config.js
@@ -2,13 +2,7 @@ import { defineConfig } from '@sunshj/eslint-config'
 
 export default defineConfig([
   {
-    rules: {
-      'vue/component-name-in-template-casing': [
-        'error',
-        'PascalCase',
-        { registeredComponentsOnly: false }
-      ]
-    }
+    rules: {}
   },
   {
     ignores: ['types/**/*.d.ts']
